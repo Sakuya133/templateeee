0854b058918161793f7851724ac879c205d987d4
Gusde Dimas <55240860+Sakuya133@users.noreply.github.com>
2025-06-26 08:47:11 +0700

    Update vektorbuatan.c

--- vektorbuatan.c
+++ vektorbuatan.c
@@ -4,38 +4,34 @@
 #include <stdbool.h>
 
 typedef struct {
-    char NRP[11];
-    char name[30];
-    int gender;    
-    float IPK;
-} Mahasiswa;
+    char ID[10];
+    char nama[30];
+    char jenisDagangan[30];
+    float omzetPerHari;
+} Pedagang;
 
 typedef struct {
-    Mahasiswa *_arr;
+    Pedagang *_arr;
     unsigned _size, _capacity;
 } VectorBuatan;
-
 
 void vector_init(VectorBuatan *vec) {
     vec->_capacity = 2u;
     vec->_size = 0u;
-    vec->_arr = (Mahasiswa *)malloc(sizeof(Mahasiswa) * vec->_capacity);
+    vec->_arr = (Pedagang *)malloc(sizeof(Pedagang) * vec->_capacity);
 }
-
 
 bool vector_isEmpty(VectorBuatan *vec) {
     return vec->_size == 0;
 }
 
-
-void vector_pushBack(VectorBuatan *vec, Mahasiswa val) {
+void vector_pushBack(VectorBuatan *vec, Pedagang val) {
     if (vec->_size >= vec->_capacity) {
         vec->_capacity *= 2;
-        vec->_arr = (Mahasiswa *)realloc(vec->_arr, sizeof(Mahasiswa) * vec->_capacity);
+        vec->_arr = (Pedagang *)realloc(vec->_arr, sizeof(Pedagang) * vec->_capacity);
     }
     vec->_arr[vec->_size++] = val;
 }
-
 
 void vector_deleteAt(VectorBuatan *vec, unsigned index) {
     if (index >= vec->_size) return;
@@ -43,6 +39,13 @@ void vector_deleteAt(VectorBuatan *vec, unsigned index) {
         vec->_arr[i] = vec->_arr[i + 1];
     }
     vec->_size--;
+}
+
+void vector_free(VectorBuatan *vec) {
+    free(vec->_arr);
+    vec->_arr = NULL;
+    vec->_size = 0;
+    vec->_capacity = 0;
 }
 
 
